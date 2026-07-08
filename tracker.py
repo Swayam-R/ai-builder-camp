ed558050c5899181f742e51fbeedffa193b9cc09
Swayam <swayamraut1708@gmail.com>
2026-07-08 15:09:24 +0400

    Add average time function

---
+++ tracker.py
@@ -0,0 +1,7 @@
+swim_times = [28.5, 27.9, 29.1, 27.4]
+def average_time(times):
+    return sum(times) / len(times)
+
+print(average_time(swim_times))
+rank_points = [1200, 1350, 1290, 1400]
+print(average_time(rank_points))

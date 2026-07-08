42307bd34b590706de0874898a6fb9340e704fb4
Swayam <swayamraut1708@gmail.com>
2026-07-08 15:21:44 +0400

    Add fastest, slowest, and count functions

--- tracker.py
+++ tracker.py
@@ -5,3 +5,14 @@ def average_time(times):
 print(average_time(swim_times))
 rank_points = [1200, 1350, 1290, 1400]
 print(average_time(rank_points))
+def fastest_time(times):
+    return min(times)
+
+print(fastest_time(swim_times))
+def slowest_time(times):
+    return max(times)
+print(slowest_time(swim_times))
+def count_time(times):
+    return len(times)
+print("The amount of times you recorded your races was")
+print (count_time(swim_times))
